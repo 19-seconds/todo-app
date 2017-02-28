bb4fa3c2f623c0f4f4c0cfab515df3a2f02c4921
Mark Richardson <markeban@yahoo.com>
2017-02-28 14:16:56 +0200

    objects in angular

--- app/assets/javascripts/todo_ctrl.js
+++ app/assets/javascripts/todo_ctrl.js
@@ -3,11 +3,40 @@
 
   angular.module("app").controller("todoCtrl", function($scope) {
 
-    $scope.tasks = ["Walk the dog", "Make dinner", "Take out the trash", "practice piano"];
+    $scope.tasks = [
+      {
+        name: "Walk the dog",
+        priority: 7,
+        completed: false
+      },
+      {
+        name: "Practice Piano",
+        priority: 1,
+        completed: false
+      },
+      {
+        name: "Clean dishes",
+        priority: 10,
+        completed: false
+      }
+    ];
 
-    $scope.addTask = function(task) {
-      $scope.tasks.push(task);
+
+    $scope.addTask = function(name, priority) {
+      if (name && priority) {
+        var task = {
+          name: name,
+          priority: priority,
+          completed: false
+        }
+        $scope.tasks.push(task);
+      }
     };
+
+    $scope.completeTask = function(task) {
+      var index = $scope.tasks.indexOf(task);
+      $scope.tasks[index].completed = true;
+    }
 
     window.$scope = $scope;
 
